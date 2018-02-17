77b0941a6b136044320bc901e37acaac48292dad
mudambirichard <mudarichards@gmail.com>
2018-02-17 14:44:34 +0300

    read python notes in da hardway

---
+++ challenge5b.py
@@ -0,0 +1,42 @@
+Python 3.5.2 (default, Nov 23 2017, 16:37:01) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> ivan['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    ivan['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+NameError: name 'ivan' is not defined
+>>> ivan['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    ivan['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+NameError: name 'ivan' is not defined
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> ivan = ['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+>>> ivan
+['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+>>> ivan[0:3]
+['Rechal', 'Rebeca', 'shamim']
+>>> ivan[2:4]
+['shamim', 'Gorret']
+>>> ivan[0:-1]
+['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah']
+>>> ivan[0:-3]
+['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene']
+>>> ivan
+['Rechal', 'Rebeca', 'shamim', 'Gorret', 'Sauda', 'Irene', 'Gloria', 'Sarah', 'Ruth']
+>>> ivan.remove(3)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    ivan.remove(3)
+ValueError: list.remove(x): x not in list
+>>> ivan.sort()
+>>> ivan
+['Gloria', 'Gorret', 'Irene', 'Rebeca', 'Rechal', 'Ruth', 'Sarah', 'Sauda', 'shamim']
+>>> ivan.sort()
+>>> hello[2:1
+      
